3e1697b621797578659cd6c856a35e54b2f522b0
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2013-03-16 18:59:33 -0500

    Tabified UserInterface

--- UI/UserInterface.h
+++ UI/UserInterface.h
@@ -8,70 +8,70 @@
 class UserInterface
 {
 private:
-    sfg::SFGUI sfgui;
-    sfg::Desktop desktop;
-    sfg::Window::Ptr window;
+	sfg::SFGUI sfgui;
+	sfg::Desktop desktop;
+	sfg::Window::Ptr window;
 
-    // Widgets...
-    sfg::Box::Ptr box;
+	// Widgets...
+	sfg::Box::Ptr box;
 
-    sfg::Label::Ptr infoLabel;
+	sfg::Label::Ptr infoLabel;
 
-    sfg::Button::Ptr quitButton;
-    sfg::Button::Ptr openButton;
+	sfg::Button::Ptr quitButton;
+	sfg::Button::Ptr openButton;
 
-    sfg::ToggleButton::Ptr saveButton;
+	sfg::ToggleButton::Ptr saveButton;
 
-    sfg::CheckButton::Ptr showColorButton;
-    sfg::CheckButton::Ptr showDepthButton;
-    sfg::CheckButton::Ptr showSkeletonButton;
+	sfg::CheckButton::Ptr showColorButton;
+	sfg::CheckButton::Ptr showDepthButton;
+	sfg::CheckButton::Ptr showSkeletonButton;
 
-    sfg::CheckButton::Ptr showPositionButton;
-    sfg::CheckButton::Ptr showJointsButton;
-    sfg::CheckButton::Ptr showInferredButton;
-    sfg::CheckButton::Ptr showOrientationButton;
-    sfg::CheckButton::Ptr showBonesButton;
-    sfg::CheckButton::Ptr showJointPathButton;
+	sfg::CheckButton::Ptr showPositionButton;
+	sfg::CheckButton::Ptr showJointsButton;
+	sfg::CheckButton::Ptr showInferredButton;
+	sfg::CheckButton::Ptr showOrientationButton;
+	sfg::CheckButton::Ptr showBonesButton;
+	sfg::CheckButton::Ptr showJointPathButton;
 
-    sfg::ComboBox::Ptr filterJointsCombo;
+	sfg::ComboBox::Ptr filterJointsCombo;
 
-    sfg::ProgressBar::Ptr jointFramesProgress;
-    sfg::Label::Ptr jointFramesFilename;
-    sfg::Label::Ptr jointFrameIndex;
+	sfg::ProgressBar::Ptr jointFramesProgress;
+	sfg::Label::Ptr jointFramesFilename;
+	sfg::Label::Ptr jointFrameIndex;
 
 public:
-    UserInterface();
+	UserInterface();
 
-    void draw(sf::RenderWindow &renderWindow);
-    void handleEvent(sf::Event &event);
+	void draw(sf::RenderWindow &renderWindow);
+	void handleEvent(sf::Event &event);
 
-    void setInfo    (const std::string &info) { infoLabel->SetText(sf::String(info)); }
-    void setFileName(const std::string &name) { jointFramesFilename->SetText(sf::String(name)); }
-    void setProgress(float fraction) { jointFramesProgress->SetFraction(fraction); }
-    void setIndex(int index) {
-        std::stringstream ss;
-        ss << "[" << index << "]";
-        jointFrameIndex->SetText(sf::String(ss.str()));
-    }
+	void setInfo    (const std::string &info) { infoLabel->SetText(sf::String(info)); }
+	void setFileName(const std::string &name) { jointFramesFilename->SetText(sf::String(name)); }
+	void setProgress(float fraction) { jointFramesProgress->SetFraction(fraction); }
+	void setIndex(int index) {
+		std::stringstream ss;
+		ss << "[" << index << "]";
+		jointFrameIndex->SetText(sf::String(ss.str()));
+	}
 
 private:
-    void setupWidgetHandlers();
-    void setupWindowConfiguration();
+	void setupWidgetHandlers();
+	void setupWindowConfiguration();
 
-    // Widget handlers...
-    void onQuitButtonClick();
-    void onOpenButtonClick();
-    void onSaveButtonClick();
-    void onShowColorButtonClick();
-    void onShowDepthButtonClick();
-    void onShowSkeletonButtonClick();
-    //void onShowPositionButtonClick();
-    //void onShowJointsButtonClick();
-    //void onShowInferredButtonClick();
-    //void onShowOrientationButtonClick();
-    //void onShowBonesButtonClick();
-    //void onShowJointPathButtonClick();
-    void onProgressBarMouseMove();
-    void onFilterComboSelect();
+	// Widget handlers...
+	void onQuitButtonClick();
+	void onOpenButtonClick();
+	void onSaveButtonClick();
+	void onShowColorButtonClick();
+	void onShowDepthButtonClick();
+	void onShowSkeletonButtonClick();
+	//void onShowPositionButtonClick();
+	//void onShowJointsButtonClick();
+	//void onShowInferredButtonClick();
+	//void onShowOrientationButtonClick();
+	//void onShowBonesButtonClick();
+	//void onShowJointPathButtonClick();
+	void onProgressBarMouseMove();
+	void onFilterComboSelect();
 };
 
